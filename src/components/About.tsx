52af95bd0d83bdafa6c31a54436d1f2eda4256fc
Kevin Amistad <amistad_k@f112.ub.lan>
2026-05-11 10:27:00 +0800

    Change layout on about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,38 @@
 import Container from 'react-bootstrap/Container';
 import Stack from 'react-bootstrap/Stack';
+import Badge from 'react-bootstrap/Badge';
+
 function About() {
-
   return (
-    <>
-      <Container>
-        <h1>About Me</h1>
-
-        <p>
-            I'm Student of University of Baguio currently as a IT student(1st Year), I'm eager to learn more about on the Front end and 
-            Back end to further my knowledge and apply it on technical fields. My skills as of now: 
-        </p>
-        <Stack gap={3}>
-        <div className="p-1"><strong>DATBASE:</strong><br /> - MySql <br /> - MongoDB</div>
-        <div className="p-1"><strong>FRONTEND:</strong><br /> - React <br /> - TypeScript</div>
-        <div className="p-1"><strong>BACKEND:</strong><br /> - Java</div>
-    </Stack>
-      </Container>
-    </>
-  )
+    <Container className="py-4">
+      <h1 className="mb-3">About Me</h1>
+      <p className="lead">
+        I'm a 1st Year IT student at the University of Baguio, eager to learn more 
+        about Front-end and Back-end development to further my knowledge and 
+        apply it in technical fields.
+      </p>
+      
+      <hr />
+      
+      <h3>Skills</h3>
+      <Stack gap={2}>
+        <div className="bg-light border p-3 rounded">
+          <strong>DATABASE:</strong><br />
+          <Badge bg="secondary" className="me-1">MySQL</Badge>
+          <Badge bg="secondary">MongoDB</Badge>
+        </div>
+        <div className="bg-light border p-3 rounded">
+          <strong>FRONTEND:</strong><br />
+          <Badge bg="secondary" className="me-1">React</Badge>
+          <Badge bg="secondary">TypeScript</Badge>
+        </div>
+        <div className="bg-light border p-3 rounded">
+          <strong>BACKEND:</strong><br />
+          <Badge bg="secondary">Java</Badge>
+        </div>
+      </Stack>
+    </Container>
+  );
 }
 
 export default About;
